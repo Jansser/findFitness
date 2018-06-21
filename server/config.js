632ca1af527fbc34f6d3d3d46f5de72e002783e8
Jansser <jansser_costa@hotmail.com>
2018-06-21 18:30:29 -0300

    buildin

--- server/config.js
+++ server/config.js
@@ -1,9 +1,10 @@
 exports.port = process.env.PORT || 3001;
 exports.origin = process.env.ORIGIN || `http://localhost:${exports.port}`;
+//callbackURL: 'http://localhost:3001/auth/facebook/callback',
 
 exports.facebook = {
   clientID: '297087374163131',
   clientSecret: 'd99a7135127a13474b0a82b0b33a414f',
-  callbackURL: 'http://localhost:3001/auth/facebook/callback',
+  callbackURL: 'https://find-fitness.herokuapp.com/auth/facebook/callback',
   profileFields: ['id', 'name', 'displayName', 'picture', 'email'],
 };
