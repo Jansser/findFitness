ba8073fe202f756710054e395b04c61656e20344
Jansser <jansser_costa@hotmail.com>
2018-06-21 16:35:51 -0300

    Postsgres config

--- server/config.js
+++ server/config.js
@@ -7,13 +7,3 @@ exports.facebook = {
   callbackURL: 'http://localhost:3001/auth/facebook/callback',
   profileFields: ['id', 'name', 'displayName', 'picture', 'email'],
 };
-
-/* 
-'facebookAuth' : {
-        'clientID'      : 'your-clientID-here',
-        'clientSecret'  : 'your-client-secret-here',
-        'callbackURL'     : 'http://localhost:4000/api/auth/facebook/callback',
-        'profileURL': 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email'
-
-    },
-*/
